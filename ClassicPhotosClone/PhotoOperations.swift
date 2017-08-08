9faae7a5c9ae9e019c07f8353669588af3f3d634
mainul <mainul@mainul.therapbd.net>
2017-08-08 15:33:08 +0600

    Added Pending Operations Class

--- ClassicPhotosClone/PhotoOperations.swift
+++ ClassicPhotosClone/PhotoOperations.swift
@@ -22,3 +22,21 @@ class PhotoRecord {
         self.url = url
     }
 }
+
+class PendingOperations {
+    lazy var downloadsInProgress = [IndexPath: Operation]()
+    lazy var downloadQueue: OperationQueue = {
+        var queue = OperationQueue()
+        queue.name = "Download queue"
+        queue.maxConcurrentOperationCount = 1
+        return queue
+    }()
+    
+    lazy var filtrationsInProgress = [IndexPath: Operation]()
+    lazy var filtrationQueue: OperationQueue = {
+        var queue = OperationQueue()
+        queue.name = "Image Filtration queue"
+        queue.maxConcurrentOperationCount = 1
+        return queue
+    }()
+}
